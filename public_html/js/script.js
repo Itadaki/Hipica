c9cb02341ae6db6e97fe2adaeb4292627a3b3df1
Diego <parca_11@yahoo.es>
2015-02-23 22:05:29 +0100

    Ultimas pinceladas

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -29,25 +29,60 @@
 function verTerminos() {
     window.open("popup_licencia.html", "Licencia", "width=600, height=450, top=100px, left=200px");
 }
+/**
+ * @description Abre un popup con el contenido de la politica de privacidad
+ */
 function verPolitica() {
     window.open("politicaprivacidad.html", "Licencia", "width=850, height=700, top=100px, left=200px");
 }
+/**
+ * @description Cierra el anuncio
+ */
 function cerrarAnuncio() {
     document.getElementById('banner').style.height = 0;
 }
+/**
+ * @description Abre la pagina de donde estamos
+ */
 function abrirMapa() {
     window.location.assign("donde_estamos.html");
 }
+/**
+ * @argument {String} cadena ID destino en el popup
+ * @description Abre el popup de ayuda
+ */
 function ayuda(cadena) {
     window.open("ayuda.html#" + cadena, "Ayuda", "width=750, height=800, top=100px, left=200px");
 }
 
-//COOKIES
-var COOKIE_NAME = 'politica_cookies';
-var COOKIE_VALUE = 'aceptada';
+/*#######
+ #COOKIES#
+ #######*/
+/**
+ * @description Nombre de la cookie para la politica de cookies
+ * @type String
+ */
+var COOKIE_POLICY_NAME = 'politica_cookies';
+/**
+ * @description Valor de la cookie para la politica de cookies
+ * @type String
+ */
+var COOKIE_POLICY_VALUE = 'aceptada';
+/**
+ * @description Nombre de la cookie para el saludo de bienvenida
+ * @type String
+ */
+var COOKIE_GREETING_NAME = 'nombre';
+/**
+ * @description Tiempo de expiracion de las cookies en dias
+ * @type Number
+ */
 var COOKIE_DAYS = 365;
+/**
+ * @description Crea el div de aviso de politica de cookies
+ */
 function crearPolitica() {
-    if (!checkCookie(COOKIE_NAME)) {
+    if (!checkCookie(COOKIE_POLICY_NAME)) {
         var cookies = document.createElement('div');
         cookies.id = 'cookies';
         cookies.innerHTML = "<div>Utilizamos cookies para ofrecer a nuestros visitantes una forma mas cómoda y eficiente a la hora de navegar por nuestra web. Para ello debes aceptar nuestra <a href='politica_cookies.html'>Política de Cookies</a></div><span onclick='aceptarPolitica()'>Aceptar</span>";
@@ -55,19 +90,28 @@ function crearPolitica() {
         body.insertBefore(cookies, body.childNodes[0]);
     }
 }
+/**
+ * @description Guarda la cookie de aceptacion de la politica
+ */
 function aceptarPolitica() {
-    setCookie(COOKIE_NAME, COOKIE_VALUE, COOKIE_DAYS);
+    setCookie(COOKIE_POLICY_NAME, COOKIE_POLICY_VALUE, COOKIE_DAYS);
     document.getElementById('cookies').style.height = 0;
 }
+/**
+ * @description Crea un div con un saludo al usuario
+ */
 function crearSaludo() {
-    if (checkCookie('nombre')) {
+    if (checkCookie(COOKIE_GREETING_NAME)) {
         var saludo = document.createElement('div');
         saludo.id = 'saludo';
-        saludo.innerHTML = "<div onclick='cerrarSaludo()'>Hola " + getCookie('nombre') + ', encantado de volver a verte!</div></div>';
+        saludo.innerHTML = "<div onclick='cerrarSaludo()'>Hola " + getCookie(COOKIE_GREETING_NAME) + ', encantado de volver a verte!</div></div>';
         var body = document.getElementsByTagName('body')[0];
         body.appendChild(saludo);
     }
 }
+/**
+ * @description Cierra el saludo y elimina las cookies
+ */
 function cerrarSaludo() {
     setCookie('nombre', '', 0);
     setCookie('apellidos', '', 0);
@@ -76,18 +120,34 @@ function cerrarSaludo() {
     saludo.style.height = 0;
     saludo.style.padding = 0;
 }
+/**
+ * @description Crea una nueva cookie
+ * @param {String} cname
+ * @param {String} cvalue
+ * @param {Number} exdays
+ */
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
 }
+/**
+ * @description Comprueba si existe la cookie
+ * @param {String} cname
+ * @returns {Boolean}
+ */
 function checkCookie(cname) {
     if (getCookie(cname) !== "") {
         return true;
     }
     return false;
 }
+/**
+ * @description Devuelve el valor de una cookie
+ * @param {String} cname
+ * @returns {String}
+ */
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
@@ -100,6 +160,7 @@ function getCookie(cname) {
     }
     return "";
 }
+
 window.onload = function () {
     crearPolitica();
     crearSaludo();
